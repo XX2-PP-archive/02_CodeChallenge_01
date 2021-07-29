56f2e64f076068876ededb71da7073a4669ebaa1
Peter <schmidt.multimedia@gmail.com>
2021-07-29 10:37:00 +0200

    Lösungen 1-3

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,56 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+
+/*********** Variante 1  ****************/
+
+// Vorteil: gute Lesbarkeit | aber viele Variablen
+
+/*
+
+// Deklaration
+let ziffer1, ziffer2;
+let zahl1, zahl2, summe;
+
+// Eingabe :: Ziffern
+ziffer1 = prompt("Bitte Zahl 1 eingeben"); // 2
+ziffer2 = prompt("Bitte Zahl 2 eingeben"); // 2
+console.log(typeof ziffer1);
+console.log(typeof ziffer2);
+
+// Typkonvertierung | Ziffer --> Zahl (string --> number)
+zahl1 = parseInt(ziffer1);
+zahl2 = parseInt(ziffer2);
+console.log(typeof zahl1);
+console.log(typeof zahl2);
+
+// Berechnung :: Summe
+summe = zahl1 + zahl2; 
+console.log("Die Summe ist: " + summe);
+
+*/
+
+/*********** Variante 2  ****************/
+
+// Kompaktere Lösung
+
+/*
+let zahl1, zahl2, summe;
+
+zahl1 = parseInt(prompt("Bitte Zahl 1 eingeben"));
+zahl2 = parseInt(prompt("Bitte Zahl 2 eingeben"));
+summe = zahl1 + zahl2;
+console.log("Die Summe ist: " + summe);
+*/
+
+/*********** Variante 3  ****************/
+// Kompakt, aber schlecht lesbar
+
+console.log
+(
+    "Die Summe ist: " + 
+   (
+    parseFloat(prompt("Bitte Zahl 1 eingeben")) +
+    parseFloat(prompt("Bitte Zahl 2 eingeben"))
+   ) 
+);
